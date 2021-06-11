0d92effed34f469c085d151d1df10aa6b3d62ed3
Daniel-Blankenship <dblankenship2015@gmail.com>
2021-06-11 11:52:07 -0400

    fixed test script

--- test_player1_win.sh
+++ test_player1_win.sh
@@ -1,2 +1,2 @@
-#source venv/bin/activate
-./venv/bin/python3 ./danielb-project0/project_0.py test ./danielb-project0/player_1_win_test.json player1_win
+source venv/bin/activate
+/home/danielb/venv/bin/python3 ./danielb-project0/project_0.py test ./danielb-project0/player_1_win_test.json player1_win
